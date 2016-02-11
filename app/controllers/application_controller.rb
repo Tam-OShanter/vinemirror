25633001cf907e0eb19b4f088f212b5ad36a0798
Jordan Jamison <jordan.jamison@live.com>
2016-02-11 16:05:59 -0400

    Replaced withrvine Master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery
+
+  def authenticate!
+    if signed_in?
+      return true
+    else
+      flash[:error] = "You aren't authorized to access this page"
+      redirect_to(root_path)
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  helper_method :current_user
+
+  def signed_in?
+    !!current_user
+  end
+  helper_method :signed_in?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,17 @@
-Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+Rvine::Application.routes.draw do
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+	root :to => 'vines#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :vines do
+    member do
+      put :upvote
+    end
+  end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  match '/auth/twitter/callback', :to => 'sessions#create'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resource :dashboard
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  root :to => 'vines#index'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
